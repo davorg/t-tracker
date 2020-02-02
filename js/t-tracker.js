bfe08b1d47531f7389c21334520e09fcd0654e6b
Dave Cross <dave@dave.org.uk>
2020-02-02 16:33:17 +0000

    Switch to horizontal line

--- js/t-tracker.js
+++ js/t-tracker.js
@@ -1,3 +1,7 @@
+Number.prototype.toFixedNumber = function(digits, base){
+  var pow = Math.pow(base||10, digits);
+  return Math.round(this*pow) / pow;
+}
 
 $(document).ready(function() {
   var now             = new Date();
@@ -15,22 +19,30 @@ function do_it(ms_now, year) {
   var ms_thru_yr     = ms_now - ms_start_of_yr;
 
   var yr_percent  = (ms_thru_yr * 100) / ms_in_yr;
+  var fmt_percent = yr_percent.toFixedNumber(0);
 
-  $('#data').html(yr_percent.toFixed(0) + '%');
+  $('#data').html(fmt_percent + '%');
+
 
   var chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
     title: {
       text: ""
     },
+    dataPointWidth: 15,
     data: [{
-      type: "pie",
-      startAngle: 270,
-      yValueFormatString: "##0\"%\"",
-      indexLabel: "{label} {y}",
+      type: "stackedBar100",
+      toolTipContent: "<b>{name}:</b> {y}%",
+      name: "Done",
       dataPoints: [
-        {y: yr_percent, label: "Done", color: "green"},
-        {y: 100 - yr_percent, label: "To do", color: "white" },
+        {y: fmt_percent, label: year, color: "green"},
+      ]
+    }, {
+      type: "stackedBar100",
+      toolTipContent: "{name}",
+      name: "",
+      dataPoints: [
+        {y: 100 - fmt_percent, label: "", color: "white"},
       ]
     }]
   });
