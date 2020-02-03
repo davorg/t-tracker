32ff35916f1dbddfe2e9fa4b5a59bde995d669ce
Dave Cross <dave@dave.org.uk>
2020-02-03 09:31:31 +0000

    Track days rather than percentages

--- js/t-tracker.js
+++ js/t-tracker.js
@@ -1,28 +1,33 @@
-Number.prototype.toFixedNumber = function(digits, base){
-  var pow = Math.pow(base||10, digits);
-  return Math.round(this*pow) / pow;
+function is_leap_year(year) {
+  return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
+
+function days_in_year(year) {
+  return is_leap_year(year) ? 366 : 365;
+}
+
+function day_of_year(date) {   // d is a Date object
+  var yn = date.getFullYear();
+  var mn = date.getMonth();
+  var dn = date.getDate();
+  var d1 = new Date(yn,  0,  1, 12, 0, 0); // noon on Jan. 1
+  var d2 = new Date(yn, mn, dn, 12, 0, 0); // noon on input date
+  var ddiff = Math.round((d2 - d1) / 864e5);
+
+  return ddiff + 1;
 }
 
 $(document).ready(function() {
-  var now             = new Date();
+  var now = new Date();
 
-  do_it(now.getTime(), now.getFullYear());
+  do_it(now);
 });
 
-function do_it(ms_now, year) {
-  var start_of_yr    = new Date(year + '/01/01 00:00:00');
-  var ms_start_of_yr = start_of_yr.getTime();
-  var end_of_yr      = new Date((year + 1) + '/01/01 00:00:00');
-  var ms_end_of_yr   = end_of_yr.getTime();
+function do_it(the_date) {
+  var days_in_curr_year = days_in_year(the_date.getFullYear());
+  var curr_day          = day_of_year(the_date);
 
-  var ms_in_yr       = ms_end_of_yr - ms_start_of_yr;
-  var ms_thru_yr     = ms_now - ms_start_of_yr;
-
-  var yr_percent  = (ms_thru_yr * 100) / ms_in_yr;
-  var fmt_percent = yr_percent.toFixedNumber(1);
-
-  $('#data').html(fmt_percent + '%');
-
+  $('#data').html("Day number " + curr_day);
 
   var chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
@@ -32,17 +37,17 @@ function do_it(ms_now, year) {
     dataPointWidth: 15,
     data: [{
       type: "stackedBar100",
-      toolTipContent: "<b>{name}:</b> {y}%",
-      name: "Done",
+      toolTipContent: "<b>{name}:</b> {y}",
+      name: "Day",
       dataPoints: [
-        {y: fmt_percent, label: year, color: "green"},
+        {y: curr_day, label: the_date.getFullYear(), color: "green"},
       ]
     }, {
       type: "stackedBar100",
       toolTipContent: "{name}",
       name: "",
       dataPoints: [
-        {y: 100 - fmt_percent, label: "", color: "white"},
+        {y: days_in_curr_year - curr_day, label: "", color: "white"},
       ]
     }]
   });
